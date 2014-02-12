56d6961dcaaf9fec3d85d53fcc05f10fe0f540b9
sophie-s <sophie899@hotmail.fr>
2014-02-12 17:44:08 +0100

    Ajout de données bis Sophie

---
+++ quizz.js
@@ -0,0 +1,10 @@
+var valider = document.getElementById("envoyer");
+var input = ????
+
+valider.onclick=function validation() {
+    if(input.value="1") {
+        alert("Bonne réponse!")
+    }else{
+        alert("Mauvaise réponse...")
+    }
+};
