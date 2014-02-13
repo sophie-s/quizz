fa8905320253fe66a6e6c77a065ed9a0e0867fcb
sophie-s <sophie899@hotmail.fr>
2014-02-13 20:49:29 +0100

    Implement ou je sais plus quoi

--- quizz.js
+++ quizz.js
@@ -1,10 +1,16 @@
 var valider = document.getElementById("envoyer");
-var input = ????
 
-valider.onclick=function validation() {
-    if(input.value="1") {
-        alert("Bonne réponse!")
+var btOui = document.getElementById("boutonOui");
+var btNon = document.getElementById("boutonNon");
+
+var bonneReponse = "1";
+
+valider.onclick=function() {
+    if(btOui.checked && btOui.value == bonneReponse) {
+        alert("Bonne réponse!");
+    }else if (btNon.checked && btNon.value == bonneReponse){
+        alert("Bonne réponse!");
     }else{
-        alert("Mauvaise réponse...")
+        alert("Mauvaise réponse...");
     }
 };
